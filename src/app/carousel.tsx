546725a9c36be266ca5a1b0e03f5e4e1d8218f7f
Oskar Laing <oredgaming@gmail.com>
2026-08-02 17:36:05 -0400

    Fixed Carousel

--- src/app/carousel.tsx
+++ src/app/carousel.tsx
@@ -7,7 +7,6 @@ export default function Carousel({
   items: string[];
   className?: string;
 }) {
-
   return (
     <div
       className={twMerge(
@@ -15,23 +14,23 @@ export default function Carousel({
       before:absolute before:left-0 before:h-full before:bg-linear-to-l before:from-transparent before:to-background before:w-20 before:z-10
       after:absolute after:right-0 after:h-full after:bg-linear-to-r after:from-transparent after:to-background after:w-20 after:z-10
     `,
-        className
+        className,
       )}
     >
       <div className="relative flex overflow-x-hidden">
         <div className="py-12 animate-marquee whitespace-nowrap">
           {items.map((item, index) => (
-          <span key={index} className={"mx-4 font-bold "}>
-            {item}
-          </span>
-        ))}
+            <span key={index} className={"mx-4 font-bold "}>
+              {item}
+            </span>
+          ))}
         </div>
         <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
           {items.map((item, index) => (
-          <span key={index} className={"mx-4 font-bold "}>
-            {item}
-          </span>
-        ))}
+            <span key={index} className={"mx-4 font-bold "}>
+              {item}
+            </span>
+          ))}
         </div>
       </div>
     </div>
